251ca7341e30a2372f987d32f2e082e79986cb44
Zsombor Acs <v-zsacs@expedia.com>
2016-03-15 11:05:41 +0100

    Message ordering demo: diff sender case.

--- src/main/scala/akka/Application.scala
+++ src/main/scala/akka/Application.scala
@@ -9,7 +9,12 @@ object Application extends App {
 
   val bankAccount = system.actorOf(Props[BankAccount], "bankAccount")
 
-  system.actorOf(Props(new DepositClient(bankAccount)), "depositClient")
+  system.actorOf(Props(new DepositClient(bankAccount, 10)), "depositClient1")
+  system.actorOf(Props(new DepositClient(bankAccount, 20)), "depositClient2")
+  system.actorOf(Props(new DepositClient(bankAccount, 30)), "depositClient3")
+  system.actorOf(Props(new DepositClient(bankAccount, 40)), "depositClient4")
+  system.actorOf(Props(new DepositClient(bankAccount, 50)), "depositClient5")
+  system.actorOf(Props(new DepositClient(bankAccount, 60)), "depositClient6")
 
   Thread.sleep(2000)
   system.shutdown()
